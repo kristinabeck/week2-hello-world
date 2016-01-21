de5b27d87cf4c811aaa6f4fe68736f6043eb6b58
Kristina Beck <kbeck1@mail.valenciacollege.edu>
2016-01-20 21:24:32 -0500

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,5 @@
-# replace the contents of this comment with your full name
+#!/usr/bin/python
+# Kristina Beck
 
 # write a program that:
 # 1. greets the user in English
@@ -7,4 +8,21 @@
 # 4. exits
 
 # make sure that your code contains comments explaining your logic!
+
+
+print ('''Hello :)    
+Choose a language and I'll greet you in that language!
+1. Spanish
+2. Dutch
+3. French ''')  # greeting in multi-line print
+
+language = input('Choose your language: ')  # request for user to choose a language
+
+if language == 1:     # greeting options : set language variable option 1
+    print 'Hola'      # printing variable assigned alias
+if language == 2:     # greeting options : set language variable option 2
+    print 'Hallo'     # printing variable assigned alias
+if language == 3:     # greeting options : set language variable option 3
+    print 'Bonjour'   # printing variable assigned alias
+
 
